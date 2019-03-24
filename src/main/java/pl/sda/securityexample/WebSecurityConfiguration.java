ec3ef2e327b1f80712ecccfc8dc37b20d2dc1d7f
Łukasz Bresz <breszlukasz@gmail.com>
2019-03-24 09:55:14 +0100

    add UserDetailService

--- src/main/java/pl/sda/securityexample/WebSecurityConfiguration.java
+++ src/main/java/pl/sda/securityexample/WebSecurityConfiguration.java
@@ -1,8 +1,12 @@
 package pl.sda.securityexample;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -13,7 +17,23 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests()
                 .anyRequest()
-                .authenticated()
+                .authenticated();
 
+    }
+
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("user1")
+//                .password("pass1")
+//                .roles("SOME_ROLE");
+//    }
+
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
